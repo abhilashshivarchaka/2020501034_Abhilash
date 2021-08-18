bca7d269910dbe5c4386cd05cbd3b538f200f616
abhilashshivarchaka <abilashshivarchaka@msitprogram.net>
2021-08-18 21:48:51 +0530

    Done

--- 02-nth_happy_prime-Python/nth_happy_prime.py
+++ 02-nth_happy_prime-Python/nth_happy_prime.py
@@ -5,4 +5,39 @@
 
 
 def fun_nth_happy_prime(n):
-	return 0
+	# return 0
+
+
+# def nth_happy_number(n):
+	found = 0
+	guess = 0
+	while(found<=n):
+		guess+=1
+		if(ishappynumber(guess)):
+			if(prime(guess)):
+				found+=1
+	return guess
+def ishappynumber(n):
+	# your code goes here
+	if(n==1 or n==7):
+		return True
+	while (n>=10):
+		n=squares(n)
+		if(n==1):
+			return True
+	return False
+
+def squares(n):
+	s= 0
+	while(n):
+		s+= (n % 10) * (n % 10)
+		n = int(n / 10)
+	return s
+
+def prime(a):
+	if (a<2):
+		return False
+	for i in range(2,a):
+		if (a%i==0):
+			return False
+	return True
