5462c9236c551894cdecd79ce65e163273c4e72d
abhilashshivarchaka <abilashshivarchaka@msitprogram.net>
2021-08-18 23:08:49 +0530

    done

--- makeMagicSquare.py
+++ makeMagicSquare.py
@@ -7,6 +7,7 @@
 # URL: https://en.wikipedia.org/wiki/Magic_square
 
 # Hints: From Geeks for Geeks.
+# source:-https://www.geeksforgeeks.org/magic-square/
 # Did you find any pattern in which the numbers are stored? 
 
 # In any magic square, the first number i.e. 1 is 
@@ -35,4 +36,42 @@
 
 def makeMagicSquare(n):
     # Your code goes here...
-    pass
+    magicSquare = [[0 for x in range(n)]
+    for y in range(n)]
+    i = n / 2
+    j = n - 1
+    num = 1
+    while num <= (n * n):
+        if i == -1 and j == n:  
+            j = n - 2
+            i = 0
+        else:
+            if j == n:
+                j = 0
+            if i < 0:
+                i = n - 1
+        if magicSquare[int(i)][int(j)]:  
+            j = j - 2
+            i = i + 1
+            continue
+        else:
+            magicSquare[int(i)][int(j)] = num
+            num = num + 1
+ 
+        j = j + 1
+        i = i - 1  
+        # print("Magic Square for n =", n)
+    # print("Sum of each row or column",
+        #   n * (n * n + 1) / 2, "\n")
+ 
+    for i in range(0, n):
+        for j in range(0, n):
+            print('%2d ' % (magicSquare[i][j]),
+                  end='')
+ 
+            
+            if j == n - 1:
+                print()
+
+makeMagicSquare(5)
+    
