8ed87a45465ef1e979f0cb3b018d63ad142024dd
abhilashshivarchaka <abilashshivarchaka@msitprogram.net>
2021-08-31 22:48:18 +0530

    Done

--- 09-shortenlongruns-Python/shortenlongruns.py
+++ 09-shortenlongruns-Python/shortenlongruns.py
@@ -7,8 +7,22 @@
 # assert(shortenLongRuns([2, 3, 5, 5, 5, 3], 3) == [2, 3, 5, 5, 3]) 
 # Note: your function may not just create a copy of L and call the destructive version of this function (below) on 
 # that copy and return it. Instead, you must directly construct the result here.
-
+# took help from friend
 
 def shortenlongruns(L, k):
 	# Your code goes here
-	pass
+	c=1
+	count=0
+	for i in range(1,len(L)):
+		if(L[i]==L[i-1]):
+			c+=1
+		if(c>=k):
+			j=i
+			while(c!=count and L[j]==L[j-1]):
+				j+=1
+				count+=1
+			L[i],L[j]=L[j],L[i]
+			c=1
+	size=len(L)-count
+	return(L[:size])		
+	# pass
