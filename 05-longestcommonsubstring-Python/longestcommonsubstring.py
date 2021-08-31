1986b086bd1eb8f689287fbf3d4ca960bf11c21b
abhilashshivarchaka <abilashshivarchaka@msitprogram.net>
2021-08-31 19:17:19 +0530

    Done

--- 05-longestcommonsubstring-Python/longestcommonsubstring.py
+++ 05-longestcommonsubstring-Python/longestcommonsubstring.py
@@ -6,7 +6,21 @@
 # If there are two or more longest common substrings, return the lexicographically smaller one (ie, just use "<" to 
 # compare the strings). So, for example:
 # longestCommonSubstring("abcABC", "zzabZZAB") returns "AB" and not "ab"
-
+# source: GeeksforGeeks
 def longestcommonsubstring(s1, s2):
     # Yourcode goes here
-    pass
+    m=len(s1)
+    n=len(s2)
+    maxl=0
+    endind=m
+    look=[[0 for i in range(n+1)] for j in range(m+1)]
+    for i in range(1,m+1):
+        for j in range(1,n+1):
+            if s1[i-1]==s2[j-1]:
+                look[i][j]=look[i-1][j-1]+1
+                if look[i][j]>=maxl:
+                    maxl=look[i][j]
+                    endind=i
+    return s1[endind-maxl:endind]
+
+    # pass
