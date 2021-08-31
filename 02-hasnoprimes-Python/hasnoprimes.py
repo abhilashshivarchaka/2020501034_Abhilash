6ac2e70498b525b191733aa87774ad8fb932bc97
abhilashshivarchaka <abilashshivarchaka@msitprogram.net>
2021-08-31 12:03:31 +0530

    Done

--- 02-hasnoprimes-Python/hasnoprimes.py
+++ 02-hasnoprimes-Python/hasnoprimes.py
@@ -3,5 +3,19 @@
 
 
 def fun_hasnoprimes(l):
+	for i in range(len(l)):
+		for j in range(len(l[i])):
+			if isprime(l[i][j]):
+				return False
 	return True
 
+		
+
+def isprime(a):
+	if (a<2):
+		return False
+	for i in range(2,a):
+		if (a%i==0):
+			return False
+	return True 
+# print(fun_hasnoprimes([[11]]))
